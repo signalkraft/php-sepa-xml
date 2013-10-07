b676f834d982c028ad33eab78fb23fac42a0ef60
Philipp Dörner <p.doerner@testbirds.de>
2013-10-07 19:53:05 +0200

    Set locale to UTF-8 for transliterated strings

--- lib/SimpleXMLExtended.php
+++ lib/SimpleXMLExtended.php
@@ -8,8 +8,9 @@ class SimpleXMLExtended extends SimpleXMLElement
 {
   public function addCData($cdata_text)
   {
-    $node= dom_import_simplexml($this); 
-    $no = $node->ownerDocument; 
-    $node->appendChild($no->createCDATASection(htmlentities(utf8_encode($cdata_text)))); 
+    $node= dom_import_simplexml($this);
+    $no = $node->ownerDocument;
+    setlocale(LC_CTYPE, 'en_US.UTF-8');
+    $node->appendChild($no->createCDATASection(iconv('UTF-8', 'ASCII//TRANSLIT', $cdata_text)));
   } 
 }
